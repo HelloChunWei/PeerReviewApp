6360f9049b22ef9af3ad25fcd1ed751ec1e3880c
Neil Kuo <kissme3131@gmail.com>
2025-05-07 21:55:25 +0800

    feat: add MDXEditor package

--- src/pages/colleague/index.tsx
+++ src/pages/colleague/index.tsx
@@ -1,13 +1,59 @@
 import React from 'react'
 import { SidebarTrigger } from '@/components/ui/sidebar'
 import { useParams } from 'react-router'
+import {
+    MDXEditor,
+    headingsPlugin,
+    listsPlugin,
+    quotePlugin,
+    thematicBreakPlugin,
+    markdownShortcutPlugin,
+    toolbarPlugin,
+    BoldItalicUnderlineToggles,
+    BlockTypeSelect,
+    InsertThematicBreak,
+    ListsToggle,
+    UndoRedo,
+    codeBlockPlugin,
+    codeMirrorPlugin,
+    CodeToggle,
+} from '@mdxeditor/editor'
+import '@mdxeditor/editor/style.css'
 
 export default function Colleague() {
     const { key } = useParams()
     return (
-        <main>
+        <main className="p-4">
             <SidebarTrigger />
-            <h1>{key}</h1>
+            <h1 className="mb-4">{key}</h1>
+            <div className="dark-theme prose dark:prose-invert max-w-none">
+                <MDXEditor
+                    onChange={(value) => {
+                        console.log(value)
+                    }}
+                    markdown="# Hello world"
+                    contentEditableClassName="prose"
+                    plugins={[
+                        headingsPlugin(),
+                        quotePlugin(),
+                        listsPlugin(),
+                        thematicBreakPlugin(),
+                        markdownShortcutPlugin(),
+                        toolbarPlugin({
+                            toolbarContents: () => (
+                                <>
+                                    <CodeToggle />
+                                    <UndoRedo />
+                                    <BoldItalicUnderlineToggles />
+                                    <BlockTypeSelect />
+                                    <InsertThematicBreak />
+                                    <ListsToggle />
+                                </>
+                            ),
+                        }),
+                    ]}
+                />
+            </div>
         </main>
     )
 }
